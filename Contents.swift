51291f627a7b73bda47fb34512940a7b34cf7d01
jbyers2 <jbyers2@gmail.com>
2016-07-11 15:03:42 -0500

    jbyers

---
+++ Contents.swift
@@ -0,0 +1,42 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+/* Create a new playground named Constants_And_Variables and do the following;
+
+Experiment - Create 3 variables one int one float and one double
+
+Experiment - Create 3 constants one int one float and one double
+
+Experiment - Create 3 explicit constants one string one float and one double
+
+Experiment - Create a constant initialize its value then change its value on the next line
+
+Experiment - Create a constant with an explicit type of Float and a value of 4 (What happens?)
+
+Answer this question in the comments of your code!
+ */
+
+
+var i:Int=1
+var f:Float=123.45
+var d:Double=987.65
+
+let ki=12
+let kf=12.3
+let kd=98.7
+
+let kei:String="explict string"
+let kef1:Float=45.678
+let ked:Double=98.765
+
+let k=111
+//get error if try to change value -- can't change value of constant
+
+let kef2:Float=4
+//created okay, inferred data type
+
+
+
